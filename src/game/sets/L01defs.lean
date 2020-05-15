cfd21701024efc4273a7b0067ba37e4ac9694a23
Aniruddh Agarwal <me@anrddh.me>
2020-05-15 14:31:21 -0400

    Fix hint for using `change' tactic in level1

--- src/game/sets/L01defs.lean
+++ src/game/sets/L01defs.lean
@@ -10,36 +10,36 @@ variable X : Type -- hide
 
 ## Level 1 : Introduction to sets.
 
-This chapter assumes you are familiar with the following tactics: `intro`, `apply`, `exact`. 
+This chapter assumes you are familiar with the following tactics: `intro`, `apply`, `exact`.
 -- TODO -- any more?
-If you are not, try playing Function World and Proposition World of the Natural Number Game. 
+If you are not, try playing Function World and Proposition World of the Natural Number Game.
 In this level we will learn about the `change` tactic, and the idea of definitional equality.
 
-For our initial examples, we'll use sets with members of a generic type. 
-Let $A$ and $B$ be sets of any type $X$. Lean defines $A \subseteq B$ to mean 
-$$\forall a, a \in A \implies a \in B.$$ 
+For our initial examples, we'll use sets with members of a generic type.
+Let $A$ and $B$ be sets of any type $X$. Lean defines $A \subseteq B$ to mean
+$$\forall a, a \in A \implies a \in B.$$
 Let's learn how to prove that $A \subseteq A$.
 
-In the lemma below, our goal is `⊢ A ⊆ A`. By *definition* this means `∀ a : X, a ∈ A → a ∈ A.` 
-This is the actual definition of `⊆` in Lean. 
-You can check this for yourself by using the `change` tactic. 
-If you start your proof with 
+In the lemma below, our goal is `⊢ A ⊆ A`. By *definition* this means `∀ a : X, a ∈ A → a ∈ A.`
+This is the actual definition of `⊆` in Lean.
+You can check this for yourself by using the `change` tactic.
+If you start your proof with
 
-`change ∀ a : ℝ, a ∈ X → a ∈ X,`
+`change ∀ a : A, a ∈ A → a ∈ A,`
 
 then the goal will change to `⊢ ∀ (a : X), a ∈ A → a ∈ A`. You can change it back with
 
 `change A ⊆ A,`
 
-The `change` tactic can be used to change the goal to something *definitionally equivalent* 
-to the goal. That is, something *equal by definition*. 
+The `change` tactic can be used to change the goal to something *definitionally equivalent*
+to the goal. That is, something *equal by definition*.
 
 To make progress with a `∀ a, ...` goal you can `intro a`. You can probably take it from here.
 Note that you do not have to use the `change` tactic at all, you can start with `intro a`.
 -/
 
 /- Lemma
-If $A$ is a set of elements of type X, then $A \subseteq A$. 
+If $A$ is a set of elements of type X, then $A \subseteq A$.
 -/
 lemma subset.refl (A : set X) : A ⊆ A :=
 begin
